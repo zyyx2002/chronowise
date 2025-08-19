4a89f75cc042323e3e323a859d29233da1d7f2b4
zyyx2002 <smartzy168@gmail.com>
2025-08-20 01:16:13 +0800

    fix: 添加ndkVersion以确保构建兼容性

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 android {
     namespace = "com.example.chronowise"
     compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
